33902e657f5972faef66ea30a3e39a7019294470
mnhut03 <dangminhnhut2005vl@gmail.com>
2025-06-27 22:29:58 +0700

    giaodien

--- FashionShop/src/components/user/Login.vue
+++ FashionShop/src/components/user/Login.vue
@@ -56,7 +56,7 @@ const login = async () => {
 </script>
 <template>
   <div class="auth-container mt-3">
-    <h2 class="auth-title">Đăng Nhập / Tạo Tài Khoản</h2>
+    <h3 class="auth-title">Đăng Nhập / Tạo Tài Khoản</h3>
     <div class="row g-4 mt-2 mb-3">
       <!-- Đăng nhập -->
       <div class="col-md-6">
