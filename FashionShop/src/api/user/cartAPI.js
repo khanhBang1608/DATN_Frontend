add44c64e737a3a8bb872082b98cc11684d6e31e
khanhBang1608 <163488048+khanhBang1608@users.noreply.github.com>
2025-07-09 22:24:13 +0700

    update url api

--- FashionShop/src/api/user/cartAPI.js
+++ FashionShop/src/api/user/cartAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/cart';
+const API_URL = 'http://localhost:8080/api/user/cart';
 const PRODUCT_API_URL = 'http://localhost:8080/api/products';
 
 const getAuthHeaders = () => {
